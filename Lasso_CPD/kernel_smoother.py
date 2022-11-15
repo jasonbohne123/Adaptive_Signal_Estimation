abb0efef7c692e75882640f8532bc0500ceb8cc6
Jason Bohne <jbohne822@gmail.com>
2022-11-14 19:24:31 -0500

    updated files

--- Lasso_CPD/kernel_smoother.py
+++ Lasso_CPD/kernel_smoother.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from scipy.sparse import dia_matrix, csc_matrix
 from scipy.sparse.linalg import inv
 from scipy.interpolate import interp1d
@@ -9,6 +8,8 @@ import sys
 def compute_kernel(x_0,x_i,bandwidth):
     """ Given two points x_0 and x_i; compute the gaussian kernel utilizing euclidean distance 
     """
+    if bandwidth==0:
+        return 0
     scale=abs((x_0-x_i)/bandwidth)
     
     weight= np.exp(-scale**2)
@@ -27,18 +28,19 @@ def kernel_smooth(reference,prior,index,bandwidth):
         denominator+=kernel
         weighted_kernel+=kernel*prior[i]
         counter+=1
+    
+    if denominator==0:
+        return reference
 
     return (weighted_kernel/denominator)
 
 def smooth_series(prior,index,bw_style=1):
-    
-    '''
+    """
     Kernel Smooths numpy array using the gaussian kerenl
-    
     i). Applies Kernel Smoothing in time based on prior 
     ii). Utilizes Scotts and Silvermans Bandwidth Selection Methods
-        
-    '''
+    """
+    
     if len(prior)!=len(index):
         print("Mismatched Series")
         return None
@@ -54,11 +56,47 @@ def smooth_series(prior,index,bw_style=1):
         smoothed_val=kernel_smooth(index[i],prior,index,bandwidth=bw)
         smooth.append(smoothed_val)
     
-    return smooth,bw
+    return np.array(smooth),bw
 
 def evaluate_kernel(y,smooth_prior,index,bandwidth):
-
-    
+    """ Evaluates kernel arbitrarily given smoothed prior 
+    """
     y_hat=kernel_smooth(y,smooth_prior,index,bandwidth)
     
     return y_hat
+
+
+def mom_kde(prior,index,N,bandwidth):
+    """ Applies robust kernel density estimation median of means 
+    """
+    kde_list=[]
+    stepsize=np.floor(len(prior)/N).astype(int)+1
+
+    for i in range(N):
+        block_prior=prior[i*stepsize:(i+1)*stepsize]
+        block_index=index[i*stepsize:(i+1)*stepsize]
+        
+        
+        if len(block_prior)==0 or len(block_index)==0:
+            continue
+        
+        # fit kde on block
+        kde,bw=smooth_series(block_prior,block_index)
+        kde_list.append(kde)
+        
+    kde_flatten=[kde_val for subkde in kde_list for kde_val in subkde]
+    
+    return np.array(kde_flatten)
+
+def cv_block_size(true,prior,index,bw,grid,verbose=False):
+    results={}
+
+    for n_i in grid:
+        kde=mom_kde(prior,index,n_i,bw)
+        mse=np.round(np.sum((true-kde)**2),2)
+        
+        results[n_i]=[mse]
+        if verbose:
+            print(f" MSE for {n_i} blocks is {mse}")
+
+    return sorted(results.items(), key=lambda x:x[1])[0]
