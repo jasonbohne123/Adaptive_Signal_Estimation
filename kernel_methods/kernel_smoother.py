29bdb424af54c459ae1c225ab993bdd93ce094a9
Jason Bohne <jbohne822@gmail.com>
2022-12-08 18:25:15 -0500

    refactored kernel code into class

--- kernel_methods/kernel_smoother.py
+++ kernel_methods/kernel_smoother.py
@@ -1,101 +1,129 @@
 import numpy as np
 from scipy.stats import iqr
 
-def compute_kernel(x_0,x_i,bandwidth):
-    """ Given two points x_0 and x_i; compute the gaussian kernel utilizing euclidean distance 
+class KernelSmoother:
+    """ Kernel Smoother Class
     """
-    if bandwidth==0:
-        return 0
-    scale=abs((x_0-x_i)/bandwidth)
-    
-    weight= np.exp(-scale**2)
-      
-    return weight
-
-def kernel_smooth(reference,prior,index,bandwidth):
-    """ Applies Kernel Smoothing in time on a reference point given prior and index
-    """
-    
-    weighted_kernel=0
-    denominator=0
-    counter=0
-    for i in range(0,len(index)):
-        kernel=compute_kernel(reference,index[i],bandwidth) 
-        denominator+=kernel
-        weighted_kernel+=kernel*prior[i]
-        counter+=1
-    
-    if denominator==0:
-        return reference
-
-    return (weighted_kernel/denominator)
-
-def smooth_series(prior,index,bw_style=1):
-    """
-    Kernel Smooths numpy array using the gaussian kerenl
-    i). Applies Kernel Smoothing in time based on prior 
-    ii). Utilizes Scotts and Silvermans Bandwidth Selection Methods
-    """
-    
-    if len(prior)!=len(index):
-        print("Mismatched Series")
-        return None
-    
-    smooth=[]
-
-    # optimal bandwidth selection are for gaussian kernels 
-    if bw_style==0:
-        bw=0.9*min(np.std(index),iqr(index)/1.35)/(len(index)**0.2) 
-    else:
-        bw=1.06*np.std(index) / (len(index)**0.2)
-    
-    
-    for i in range(0,len(prior)):
-        smoothed_val=kernel_smooth(index[i],prior,index,bandwidth=bw)
-        smooth.append(smoothed_val)
-    
-    return np.array(smooth),bw
-
-def evaluate_kernel(y,smooth_prior,index,bandwidth):
-    """ Evaluates kernel arbitrarily given smoothed prior 
-    """
-    y_hat=kernel_smooth(y,smooth_prior,index,bandwidth)
-    
-    return y_hat
+    def __init__(self, prior, index, bandwidth_style):
+        self.prior = prior
+        self.index = index
+        self.bandwidth_style = bandwidth_style
 
 
-def mom_kde(prior,index,N,bandwidth):
-    """ Applies robust kernel density estimation median of means 
-    """
-    kde_list=[]
-    stepsize=np.floor(len(prior)/N).astype(int)+1
-
-    for i in range(N):
-        block_prior=prior[i*stepsize:(i+1)*stepsize]
-        block_index=index[i*stepsize:(i+1)*stepsize]
+    def compute_kernel(self,x_0,x_i,bandwidth):
+        """ Given two points x_0 and x_i; compute the gaussian kernel utilizing euclidean distance 
+        """
+        if bandwidth==0:
+            return 0
+        scale=abs((x_0-x_i)/bandwidth)
         
+        weight= np.exp(-(scale**2))
         
-        if len(block_prior)==0 or len(block_index)==0:
-            continue
-        
-        # fit kde on block
-        kde,bw=smooth_series(block_prior,block_index)
-        kde_list.append(kde)
-        
-    kde_flatten=[kde_val for subkde in kde_list for kde_val in subkde]
-    
-    return np.array(kde_flatten)
+        return weight
 
-def cv_block_size(true,prior,index,bw,grid,verbose=False):
-    """ Cross validates block size for kernel density estimation"""
-    results={}
+    def fit(self):
+        """
+        Fits a kernel smoothing estimator on a prior series
+        """
 
-    for n_i in grid:
-        kde=mom_kde(prior,index,n_i,bw)
-        mse=np.round(np.sum((true-kde)**2),2)
+        if len(self.prior)!=len(self.index):
+            print("Mismatched Series")
+            return None
         
-        results[n_i]=[mse]
-        if verbose:
-            print(f" MSE for {n_i} blocks is {mse}")
+        kernel_matrix=np.zeros(len(self.index))
+        
+        # optimal bandwidth selection are for gaussian kernels 
+        if self.bandwidth_style==0:
+            bw=0.9*min(np.std(self.index),iqr(self.index)/1.35)/(len(self.index)**0.2) 
+        else:
+            bw=1.06*np.std(self.index) / (len(self.index)**0.2)
 
-    return sorted(results.items(), key=lambda x:x[1])[0]
+
+        for i in range(0,len(self.index)):
+            denominator=0
+            weight=0
+
+            for j in range(0,len(self.index)):
+                kernel=self.compute_kernel(self.index[i],self.index[j],bw) 
+                
+                # normalize kernel matrix to unit density
+                denominator+=kernel
+                weight+=kernel*self.prior[j]
+            
+            kernel_matrix[i]=weight/denominator
+
+        self.fitted_kernel_matrix=kernel_matrix
+        return 
+
+    def smooth_series(self):
+        """ Smooths the series using the kernel smoothing estimator 
+        """
+
+        
+        self.smooth_prior=self.fitted_kernel_matrix
+        
+        return
+
+
+class MomKernelSmoother(KernelSmoother):
+    """ Median of Means Kernel Smoother Class
+    """
+    def __init__(self,kernel_smoother):
+        self.prior=kernel_smoother.prior
+        self.index=kernel_smoother.index
+        self.bandwidth_style=kernel_smoother.bandwidth_style
+        self.fitted_kernel_matrix=kernel_smoother.fitted_kernel_matrix
+
+
+    def partition_blocks(self,prior,index,N):
+        """ Partition prior into N blocks
+        """
+        all_indices=np.arange(len(index))
+        partition_indices=np.array_split(all_indices,N)
+
+        blocked_prior=[]
+        blocked_index=[]
+        for i in range(N):
+            blocked_prior.append(prior[partition_indices[i]])
+            blocked_index.append(index[partition_indices[i]])
+            
+        return blocked_prior,blocked_index
+
+
+    def mom_kde(self,prior,index,N,bandwidth):
+        """ Applies robust kernel density estimation median of means 
+        """
+        kde_list=[]
+        
+        # partition prior into N blocks
+        block_prior,block_index=self.partition_blocks(prior,index,N)
+
+        # fit kde on each block
+        kde_estimates=np.empty((N,len(index)))
+        
+        for i in range(N):
+            # determine the smooth values for each block
+            kde_estimates[i]=smooth_series(block_prior[i],block_index[i])[0]
+
+        # take median of estimates across each block
+        kde=np.median(kde_estimates,axis=0)
+
+        # rescale to unit density
+        kde=kde/np.sum(kde)
+
+        return kde 
+        
+
+    def cv_block_size(self,true,prior,index,bw,grid,verbose=False):
+        """ Cross validates block size for kernel density estimation"""
+        results={}
+
+        for n_i in grid:
+            kde=self.mom_kde(prior,index,n_i,bw)
+            mse=np.round(np.sum((true-kde)**2),2)
+            
+            results[n_i]=[mse]
+            if verbose:
+                print(f" MSE for {n_i} blocks is {mse}")
+
+        return sorted(results.items(), key=lambda x:x[1])[0]
