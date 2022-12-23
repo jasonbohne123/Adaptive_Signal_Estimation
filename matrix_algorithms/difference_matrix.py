f73b0bb11cd96d71cec8f86b0d2088989eb2b929
Jason Bohne <jbohne822@gmail.com>
2022-12-22 19:42:56 -0500

    fixed DDT matrix

--- matrix_algorithms/difference_matrix.py
+++ matrix_algorithms/difference_matrix.py
@@ -1,41 +1,42 @@
-
-
 import pentapy as pp
 import numpy as np
-from scipy.sparse import dia_matrix
+from scipy.sparse import spdiags, csr_matrix, dia_matrix
 from scipy.sparse.linalg import spsolve
-from scipy.linalg import get_lapack_funcs  
-
+from scipy.linalg import get_lapack_funcs
 
 
 class Difference_Matrix():
-    def __init__(self,n,k,style=None) -> None:
-        
+    def __init__(self, n, k, style=None) -> None:
 
-        self.n=n
-        self.k=k
-        self.style=style if style is not None else "lapack"
+        self.n = n
+        self.k = k
+        self.style = style if style is not None else "lapack"
 
         # number of upper and lower diagonals of DDT
-        self.l=k
-        self.u=k
+        self.l = k
+        self.u = k
 
-        # create the difference matrix
-        diff_diag=self.extract_matrix_diagonals(self.n,self.k)
-        self.D=dia_matrix((diff_diag, np.arange(-(k-1), self.k+1)), shape=(self.n, self.n-2)).toarray()
+        # create the difference matrix (sparse)
+        D = self.extract_matrix_diagonals(self.n, self.k)
 
-        # extract only the diagonals of the DDT matrix (which is pascals(k+2))
-        self.diag=self.extract_matrix_diagonals(self.n,self.k+2)
+        # save the difference matrix
+        self.D = D.toarray()
 
-        # save the DDT matrix 
-        self.DDT=dia_matrix((self.diag, np.arange(-self.l, self.u + 1)), shape=(self.n, self.n)).toarray()
-        
+        # create the DDT matrix (sparse)
+        DDT = D.dot(D.T)
+
+        # determine the projected coefficients across diagonals
+        DDT_diag_coeff = [DDT.diagonal(i)[0] for i in range(-k, k+1)]
+
+        self.DDT_diag = np.array([i*np.ones(n-2) for i in DDT_diag_coeff])
+
+        # save the DDT matrix
+        self.DDT = DDT.toarray()
+
         # save the inverse of the DDT matrix
-        self.DDT_inv=self.invert(style=self.style)
-      
-    
-    
-    def invert(self,style):
+        self.DDT_inv = self.invert(style=self.style)
+
+    def invert(self, style):
         """
         Inverts the difference matrix
 
@@ -49,20 +50,20 @@ class Difference_Matrix():
         DDT_inv : Array
             Inverse of the difference matrix
         """
-        if style=="lapack":
-            DDT_inv=self.LU_decomposition()
+        if style == "lapack":
+            DDT_inv = self.LU_decomposition()
             return DDT_inv
-        elif style=="pentapy":
-            DDT_inv=self.ptrans_algorithm()
+        elif style == "pentapy":
+            DDT_inv = self.ptrans_algorithm()
             return DDT_inv
-        elif style=="sparse":
-            DDT_inv=self.sparse_banded()
+        elif style == "sparse":
+            DDT_inv = self.sparse_banded()
             return DDT_inv
         else:
-            
+
             return None
 
-    def LU_decomposition(self,b=None):
+    def LU_decomposition(self, b=None):
         """
         LU decomposition specifically for banded matrices using LAPACK routine gbsv
 
@@ -83,18 +84,19 @@ class Difference_Matrix():
 
         # setup identity matrix
         if b is None:
-            b = np.eye(self.n)
+            b = np.eye(self.n-2)
         else:
             b = np.asarray(b)
 
-        (nlower, nupper) = self.l,self.u # number of lower and upper diagonals
+        # number of lower and upper diagonals
+        (nlower, nupper) = self.l, self.u
 
         # get lapack function
-        gbsv, = get_lapack_funcs(('gbsv',), (self.diag, b))
+        gbsv, = get_lapack_funcs(('gbsv',), (self.DDT_diag, b))
 
         # setup problem
-        a2 = np.zeros((2*nlower + nupper + 1, self.n), dtype=gbsv.dtype)
-        a2[nlower:, :] = self.diag
+        a2 = np.zeros((2*nlower + nupper + 1, self.n-2), dtype=gbsv.dtype)
+        a2[nlower:, :] = self.DDT_diag
         lu, piv, x, info = gbsv(nlower, nupper, a2, b, overwrite_ab=True,
                                 overwrite_b=True)
 
@@ -109,22 +111,24 @@ class Difference_Matrix():
         inv : Array
             Inverse of the difference matrix
         """
-        inv=spsolve(self.DDT,np.eye(self.n))
-        return inv
 
+        # confirm this works
+        sparse_mat = dia_matrix(
+            (self.DDT_diag, range(-self.k, self.k+1)), shape=(self.n-2, self.n-2))
+        inv = spsolve(sparse_mat, np.eye(self.n-2))
+        return inv
 
     def ptrans_algorithm(self):
         """Solves pentadiagonal system using pentapy package"""
 
-        inv=np.zeros((self.n,self.n))
-        for i in range(0,self.n):
-            unit=np.zeros(self.n)
-            unit[i]=1
-            inv[i] =pp.solve(self.DDT, unit, is_flat=False)
+        inv = np.zeros((self.n-2, self.n-2))
+        for i in range(0, self.n-2):
+            unit = np.zeros(self.n-2)
+            unit[i] = 1
+            inv[i] = pp.solve(self.DDT, unit, is_flat=False)
         return inv
 
-
-    def extract_matrix_diagonals(self,n,k):
+    def extract_matrix_diagonals(self, n, k):
         """
         Extracts only the diagonals of the difference matrix
         """
@@ -140,39 +144,40 @@ class Difference_Matrix():
         coeff = pascals(k)
         coeff = [i for i in coeff if i != 0]
         coeff = [coeff[i] if i % 2 == 0 else -coeff[i]
-                for i in range(0, len(coeff))]
+                 for i in range(0, len(coeff))]
 
         if k == 0:
-            diag=np.ones(n)
+            diag = spdiags([np.ones(n)], [0], n, n)
         else:
-            diag=np.vstack([i * np.ones(n) for i in coeff])
- 
+
+            diag = spdiags([i*np.ones(n)
+                           for i in coeff], np.arange(0, k+1), n-2, n)
+
         return diag
 
 
-def test_lapack(n=100,sig_figs=10):
+def test_lapack(n=100, sig_figs=10):
     """Test the LU decomposition method"""
-    k=2
+    k = 2
     print("Testing LU decomposition method")
-    D=Difference_Matrix(n,k,style="lapack")
-    
+    D = Difference_Matrix(n, k, style="lapack")
+
     return
 
-def test_sparse(n=100,sig_figs=10):
+
+def test_sparse(n=100, sig_figs=10):
     """Test the sparse method"""
-    k=2
+    k = 2
     print("Testing sparse method")
-    D=Difference_Matrix(n,k,style="sparse")
-    
+    D = Difference_Matrix(n, k, style="sparse")
+
     return
 
-def test_pentapy(n=100,sig_figs=10):
+
+def test_pentapy(n=100, sig_figs=10):
     """Test the pentapy method"""
-    k=2
+    k = 2
     print("Testing pentapy method")
-    D=Difference_Matrix(n,k,style="pentapy")
- 
-    return 
+    D = Difference_Matrix(n, k, style="pentapy")
 
-
-
+    return
