e53c1c07f16e7b8569f36af2f521b00080d572c9
Jason Bohne <jbohne822@gmail.com>
2023-01-03 15:40:52 -0500

    adds new files

--- trend_filtering/adaptive_tf.py
+++ trend_filtering/adaptive_tf.py
@@ -190,21 +190,3 @@ def adaptive_tf(y, t=None, lambda_p=1.0, k=2, verbose=True):
         status = "maxiter exceeded"
         print(status)
         return x, status, gap
-
-
-def cv_tf_penalty(y, grid, t=None, verbose=True):
-    """Cross Validation for constant TF penalty parameter lambda_p"""
-
-    best_gap = np.inf
-    best_lambda = None
-
-    # construct D matrix and inverse ; pass into the algorithm as a parameter
-
-    for lambda_i in grid:
-        x, status, gap = adaptive_tf(y, t, [lambda_i], verbose=verbose)
-
-        if gap < best_gap:
-            best_gap = gap
-            best_lambda = lambda_i
-
-    return np.array([best_lambda]), np.array([best_gap])
