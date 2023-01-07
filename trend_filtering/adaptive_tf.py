6ffc8b8f23cbd76aa86158e96ec8506b9edd0ff1
Jason Bohne <jbohne822@gmail.com>
2023-01-07 16:48:34 -0500

    removed difference matrix

--- trend_filtering/adaptive_tf.py
+++ trend_filtering/adaptive_tf.py
@@ -1,45 +1,10 @@
 import numpy as np
-from scipy.sparse import dia_matrix
 
 from matrix_algorithms.difference_matrix import Difference_Matrix
 from trend_filtering.opt_params import get_hyperparams
 
 
-def adjust_penalty_time(lambda_p, times, k, verbose):
-    """Adjusts penalty by difference in time between observations"""
-
-    if times is None:
-        if verbose:
-            print("No time information provided, using default penalty")
-        return lambda_p
-
-    if k != 2:
-        if verbose:
-            print("Time information provided, but k is not 2, using default penalty")
-        return lambda_p
-
-    n = len(times)
-    t_diff = np.diff(times)
-
-    # forms three tridiagonal of time penalties in linear trend filtering
-    t_diff1 = np.fmax(0.01, np.pad(t_diff, (0, 1), "constant", constant_values=t_diff[-1]))
-    t_diff3 = np.fmax(0.01, np.pad(t_diff, (0, 1), "constant", constant_values=t_diff[-1]))
-    t_diff2 = t_diff1 + t_diff3
-
-    a = t_diff1 / t_diff3
-    b = -t_diff2 / (t_diff1 * t_diff3)
-    c = t_diff3 / t_diff1
-
-    # constructs a banded matrix of time differences
-    T = dia_matrix((np.vstack([a, b, c]), [0, 1, 2]), shape=(n - 2, n - 2)).toarray()
-
-    # Scales our penalty by the time differences
-    lambda_p = np.dot(lambda_p.T, abs(T)).reshape(-1, 1)
-
-    return lambda_p
-
-
-def adaptive_tf(y, t=None, lambda_p=1.0, k=2, verbose=True):
+def adaptive_tf(y, D_=Difference_Matrix, lambda_p=1.0, k=2, verbose=True):
     """
     Adaptive trend filtering algorithm
     """
@@ -59,13 +24,9 @@ def adaptive_tf(y, t=None, lambda_p=1.0, k=2, verbose=True):
     if len(lambda_p) == 1:
         lambda_p = np.array(lambda_p) * np.ones((m, 1))
 
-    lambda_p = adjust_penalty_time(lambda_p, t, k, verbose)
-
-    # construct difference matrix
-    diff_mat = Difference_Matrix(n, k)
-    D = diff_mat.D
-    DDT = diff_mat.DDT
-    DDT_inv = diff_mat.DDT_inv
+    D = D_.D
+    DDT = D_.DDT
+    DDT_inv = D_.DDT_inv
 
     Dy = np.dot(D, y)
 
--- trend_filtering/test_adaptive_tf.py
+++ trend_filtering/test_adaptive_tf.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from matrix_algorithms.difference_matrix import Difference_Matrix
 from trend_filtering.adaptive_tf import adaptive_tf
 
 
-def test_adaptive_tf(x, n=None, plot=False):
+def test_adaptive_tf(x, n=None, k=2, plot=False):
     """Test adaptive_tf function"""
 
     # generate signal
     if n is None:
         n = len(x)
 
+    D = Difference_Matrix(n, k)
+
     x = x[:n]
 
     lambda_p = np.array([1.0])
     # reconstruct signal
-    x_hat, status, gap = adaptive_tf(x.reshape(-1, 1), lambda_p=lambda_p, verbose=False)
+    x_hat, status, gap = adaptive_tf(x.reshape(-1, 1), D_=D, lambda_p=lambda_p, verbose=False)
 
     if plot:
         # plot
