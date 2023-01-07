dfbdb3877aebc99c120bb3c4363eaccbdc60d475
Jason Bohne <jbohne822@gmail.com>
2023-01-07 17:07:27 -0500

    mvp step sizegit add .

--- trend_filtering/adaptive_tf.py
+++ trend_filtering/adaptive_tf.py
@@ -68,61 +68,10 @@ def adaptive_tf(y, D_=Difference_Matrix, lambda_p=1.0, k=2, verbose=True):
             x = y - np.dot(D.transpose(), z)
             return x, status, gap
 
-        # Update scheme for mu
-        if step >= 0.2:
-            mu_inc = max(2 * m * mu / gap, 1.2 * mu_inc)
-
-        # step size of dual variable for equality
-        rz = DDTz - w
-
-        S = DDT - np.diag((mu1 / f1 + mu2 / f2).flatten())
-        S_inv = np.linalg.inv(S)
-
-        r = -DDTz + Dy + (1 / mu_inc) / f1 - (1 / mu_inc) / f2
-        dz = np.dot(S_inv, r)
-
-        # step size for the dual variables formulated from constraints
-        dmu1 = -(mu1 + ((1 / mu_inc) + dz * mu1) / f1)
-        dmu2 = -(mu2 + ((1 / mu_inc) - dz * mu2) / f2)
-
-        # residual of dual variables
-        resDual = rz
-        resCent = np.vstack((-mu1 * f1 - 1 / mu_inc, -mu2 * f2 - 1 / mu_inc))
-        residual = np.vstack((resDual, resCent))
-
-        negIdx1 = dmu1 < 0
-        negIdx2 = dmu2 < 0
-
-        step = 1
-        if negIdx1.any():
-            step = min(step, 0.99 * min(-mu1[negIdx1] / dmu1[negIdx1]))
-        if negIdx2.any():
-            step = min(step, 0.99 * min(-mu2[negIdx2] / dmu2[negIdx2]))
-
-        # Backtracking style line search, parameterized by alpha and beta
-        for liter in range(maxlsiter):
-
-            # update params within linesearch
-            newz = z + step * dz
-            newmu1 = mu1 + step * dmu1
-            newmu2 = mu2 + step * dmu2
-            newf1 = newz - lambda_p
-            newf2 = -newz - lambda_p
-
-            print(newf1.shape, newf2.shape)
-
-            newResDual = np.dot(DDT, newz) - Dy + newmu1 - newmu2
-            newResCent = np.vstack((-newmu1 * newf1 - 1 / mu_inc, -newmu2 * newf2 - 1 / mu_inc))
-            newResidual = np.vstack((newResDual, newResCent))
-
-            # break out if actual reduction meets expected via norm of residual
-            if max(max(newf1), max(newf2)) < 0 and (
-                np.linalg.norm(newResidual) <= (1 - alpha * step) * np.linalg.norm(residual)
-            ):
-                break
-
-            step *= beta
-
+        # update step
+        newz, newmu1, newmu2, newf1, newf2 = update_step(
+            DDT, DDTz, Dy, lambda_p, z, w, mu1, mu2, f1, f2, mu, mu_inc, step, gap, m, alpha, beta, maxlsiter
+        )
         # adaptive stepsize of mu with ratio gamma
 
         newmu1, newmu2 = adaptive_step_size(pobj1, pobj2, newmu1, newmu2, gamma)
@@ -155,6 +104,62 @@ def compute_objective(DDT_inv, Dy, DTz, DDTz, z, w, mu1, mu2, lambda_p):
     dobj = -0.5 * np.dot(DTz.transpose(), DTz) + np.dot(Dy.transpose(), z)
     gap = pobj - dobj
     return pobj1, pobj2, dobj, gap
+
+
+def update_step(DDT, DDTz, Dy, lambda_p, z, w, mu1, mu2, f1, f2, mu, mu_inc, step, gap, m, alpha, beta, maxlsiter):
+    # Update scheme for mu
+    if step >= 0.2:
+        mu_inc = max(2 * m * mu / gap, 1.2 * mu_inc)
+
+    # step size of dual variable for equality
+    rz = DDTz - w
+
+    S = DDT - np.diag((mu1 / f1 + mu2 / f2).flatten())
+    S_inv = np.linalg.inv(S)
+
+    r = -DDTz + Dy + (1 / mu_inc) / f1 - (1 / mu_inc) / f2
+    dz = np.dot(S_inv, r)
+
+    # step size for the dual variables formulated from constraints
+    dmu1 = -(mu1 + ((1 / mu_inc) + dz * mu1) / f1)
+    dmu2 = -(mu2 + ((1 / mu_inc) - dz * mu2) / f2)
+
+    # residual of dual variables
+    resDual = rz
+    resCent = np.vstack((-mu1 * f1 - 1 / mu_inc, -mu2 * f2 - 1 / mu_inc))
+    residual = np.vstack((resDual, resCent))
+
+    negIdx1 = dmu1 < 0
+    negIdx2 = dmu2 < 0
+
+    step = 1
+    if negIdx1.any():
+        step = min(step, 0.99 * min(-mu1[negIdx1] / dmu1[negIdx1]))
+    if negIdx2.any():
+        step = min(step, 0.99 * min(-mu2[negIdx2] / dmu2[negIdx2]))
+
+    # Backtracking style line search, parameterized by alpha and beta
+    for liter in range(maxlsiter):
+
+        # update params within linesearch
+        newz = z + step * dz
+        newmu1 = mu1 + step * dmu1
+        newmu2 = mu2 + step * dmu2
+        newf1 = newz - lambda_p
+        newf2 = -newz - lambda_p
+
+        newResDual = np.dot(DDT, newz) - Dy + newmu1 - newmu2
+        newResCent = np.vstack((-newmu1 * newf1 - 1 / mu_inc, -newmu2 * newf2 - 1 / mu_inc))
+        newResidual = np.vstack((newResDual, newResCent))
+
+        # break out if actual reduction meets expected via norm of residual
+        if max(max(newf1), max(newf2)) < 0 and (
+            np.linalg.norm(newResidual) <= (1 - alpha * step) * np.linalg.norm(residual)
+        ):
+            break
+
+        step *= beta
+    return newz, newmu1, newmu2, newf1, newf2
 
 
 def adaptive_step_size(pobj1, pobj2, newmu1, newmu2, gamma):
