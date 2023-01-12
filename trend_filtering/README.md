d7eb9ad648af9346a76ec1884d712bb4968203ce
Jason Bohne <jbohne822@gmail.com>
2023-01-12 13:29:34 -0500

    demeaned adaptive penalty and ideas

--- trend_filtering/README.md
+++ trend_filtering/README.md
@@ -12,5 +12,14 @@ Big Idea: Determine Linear Trend in a time series. Can be used to identify chang
 
 ### Ideas for Simulation Design
 - Examine performance across a range of SNR values (sample_varaince)
+- For fixed distributions across grid of variance
+    - Performance across distribution of samples from true process
+- Quantifying the optimality of piecewise constant vs piecewise linear?
 - Across a grid of grid sizes. How important is a large candidate grid in CV
 - Across a range of system sizes
+
+
+### Ideas
+- Extension for Multivariate Trend Filtering (Across Lattices)
+- How does this algorithm look in terms of the primal-dual?
+- Can our model be easily extended?
--- trend_filtering/cv_tf.py
+++ trend_filtering/cv_tf.py
@@ -7,20 +7,27 @@ from trend_filtering.adaptive_tf import adaptive_tf
 
 
 def cross_validation(
-    y: np.ndarray, D: Difference_Matrix, grid: np.ndarray, lambda_p:Union[None,np.ndarray]=None,t: Union[None, np.ndarray] = None, verbose=True
+    y: np.ndarray,
+    D: Difference_Matrix,
+    grid: np.ndarray,
+    lambda_p: Union[None, np.ndarray] = None,
+    t: Union[None, np.ndarray] = None,
+    verbose=True,
 ):
     """Cross Validation for constant TF penalty parameter lambda_p"""
 
     best_gap = np.inf
     best_lambda = None
     for lambda_i in grid:
-        
+
         if lambda_p is not None:
-            
+
             # must be multivariate ndarray if not None
             assert isinstance(lambda_p, np.ndarray)
 
-            lambda_i = lambda_p*lambda_i
+            # scale penalty to have mean of optimal lambda
+            # is mean the best statistic here?
+            lambda_i = lambda_p * lambda_i / np.mean(lambda_p)
 
         result = adaptive_tf(y.reshape(-1, 1), D, t, lambda_p=lambda_i)
 
