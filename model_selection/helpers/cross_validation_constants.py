f54cb210c3ac097122e58aa4beb04be673af7aac
Jason Bohne <jbohne822@gmail.com>
2023-03-31 15:50:34 -0400

    add preprocessing, features, and cv update

--- model_selection/helpers/cross_validation_constants.py
+++ model_selection/helpers/cross_validation_constants.py
@@ -10,8 +10,8 @@
 def get_cv_constants():
     return {
         "k_folds": 3,
-        "cv_grid_lb": 1e-4,
-        "grid_size": 15,
+        "cv_grid_lb": 1e-3,
+        "grid_size": 10,
         "grid_spacing": "log",
         "warm_start": False,
         "verbose": True,
